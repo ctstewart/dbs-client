82fcd20f95825c48fce3fa6c8725b32b852fb40b
Christian Stewart <heeler_cs@hotmail.com>
2019-07-16 19:36:33 +0000

    Fixed dev server.

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,15 @@
 module.exports = {
   publicPath: '',
+  outputDir: process.env.NODE_ENV === 'production'
+  ? '../public'
+  : './dist',
+  devServer: {
+    host: '0.0.0.0',
+    https: true,
+    port: process.env.DEV_SERVER_PORT,
+    public: process.env.DEV_SERVER_PUBLIC,
+    proxy: process.env.API_URL
+  },
   pages: {
     'index': {
       entry: './src/index/main.js',
