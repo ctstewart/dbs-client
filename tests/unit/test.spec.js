50c529cac5fb80ac4af27ba7424d2741b132d1c2
Christian Stewart <heeler_cs@hotmail.com>
2019-08-11 03:00:38 +0000

    Added DPP to the plan test.

--- tests/unit/test.spec.js
+++ tests/unit/test.spec.js
@@ -2,7 +2,7 @@ import { expect } from 'chai'
 import { mount } from '@vue/test-utils'
 import LayoutOption from '@/components/layout/LayoutOption'
 import LocalStorageMock from './LocalStorageMock'
-global.localStorage = new LocalStorageMock
+global.localStorage = new LocalStorageMock()
 
 describe('LayoutOption', () => {
   it('tests to see if SectionPlan returns Add Phones', () => {
@@ -16,8 +16,10 @@ describe('LayoutOption', () => {
     wrapper.vm.mainObject.autopay = true
     wrapper.vm.mainObject.militaryNew = true
     wrapper.vm.mainObject.lineAccess[0].value = 1
+    wrapper.vm.mainObject.existingDPPValues[0].value = 25
+    wrapper.vm.mainObject.existingDPPValues[3].value = 25
+    wrapper.vm.mainObject.existingCreditValues[7].value = 25
     // expect(wrapper.text()).to.include(msg)
-   expect(wrapper.vm.total).to.equal('173.50') 
+    expect(wrapper.vm.total).to.equal('198.50')
   })
 })
-
