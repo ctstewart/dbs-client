badb4110dee1cfe12ba83caaed0de1f89bd6c216
Christian Stewart <heeler_cs@hotmail.com>
2019-08-11 03:18:43 +0000

    Changed one of my tests.

--- tests/unit/test.spec.js
+++ tests/unit/test.spec.js
@@ -16,10 +16,11 @@ describe('LayoutOption', () => {
     wrapper.vm.mainObject.autopay = true
     wrapper.vm.mainObject.militaryNew = true
     wrapper.vm.mainObject.lineAccess[0].value = 1
+    wrapper.vm.mainObject.lineAccess[1].value = 1
     wrapper.vm.mainObject.existingDPPValues[0].value = 25
     wrapper.vm.mainObject.existingDPPValues[3].value = 25
     wrapper.vm.mainObject.existingCreditValues[7].value = 25
     // expect(wrapper.text()).to.include(msg)
-    expect(wrapper.vm.total).to.equal('198.50')
+    expect(wrapper.vm.total).to.equal('215.50')
   })
 })
