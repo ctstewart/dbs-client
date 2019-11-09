a57bf8616c6cb22b562404ef414fcad503cc01ee
Christian Stewart <heeler_cs@hotmail.com>
2019-11-09 01:27:47 +0000

    Removed unused code. JWT is now moved to a mixin.

--- src/app.vue
+++ src/app.vue
@@ -1,86 +1,32 @@
 <template>
-  <!-- <component v-bind:is="currentPage" v-bind:currentPage="currentPage" v-on:change-page="currentPage = $event"></component> -->
-  <router-view/>
+    <router-view/>
 </template>
 
 <script>
 import axios from 'axios'
 
 export default {
-  name: 'app',
-  data: function () {
-    return {
-      jwtoken: ''
-    }
-  },
-
-  created () {
-    /* if (localStorage.getItem('jwtoken')) {
-      try {
-        this.jwtoken = JSON.parse(localStorage.getItem('jwtoken'))
-        axios({
-          method: 'get',
-          url: '/api/users/checkAuth',
-          headers: {
-            authorization: 'Bearer ' + this.jwtoken
-          }
-        })
-        .then((response) => {
-          this.currentPage = 'PageBenefitSheet'
-          setInterval(() => {
-            console.log('Interval Running')
-            axios({
-              method: 'get',
-              url: '/api/users/checkAuth',
-              headers: {
-                authorization: 'Bearer ' + this.jwtoken
-              }
-            })
-            .then((response) => {
-              console.log('token checked')
-            })
-            .catch((error) => {
-              console.log('check token failed')
-              localStorage.clear()
-              this.currentPage = 'PageLogin'
-            })
-          }, 300000)
-        })
-        .catch((error) => {
-          localStorage.clear()
-          this.currentPage = 'PageLogin'
-        })
-      } catch (e) {
-        // SHOULD I BE USING LOCALSTORAGE.CLEAR() INSTEAD?
-        // SHOULD I REFRESH THE TOKEN SILENTLY UNLESS THE BROWSER IS CLOSED?
-        localStorage.clear()
-        this.currentPage = 'PageLogin'
-      }
-    } else {
-      localStorage.clear()
-      this.currentPage = 'PageLogin'
-    } */
-  }
+    name: 'app',
 }
 </script>
 
 <style>
 html {
-  height: 100%;
+    height: 100%;
 }
 
 body {
-  height: 100%;
-  margin: 0;
-  font-family: 'Montserrat', sans-serif;
-  background-repeat: no-repeat;
-  background-attachment: fixed;
-  /* Permalink - use to edit and share this gradient: http://colorzilla.com/gradient-editor/#0a6ada+0,2fd22c+100 */
-  background: #0a6ada; /* Old browsers */
-  background: -moz-linear-gradient(45deg, #0a6ada 0%, #2fd22c 100%); /* FF3.6-15 */
-  background: -webkit-linear-gradient(45deg, #0a6ada 0%,#2fd22c 100%); /* Chrome10-25,Safari5.1-6 */
-  background: linear-gradient(45deg, #0a6ada 0%,#2fd22c 100%); /* W3C, IE10+, FF16+, Chrome26+, Opera12+, Safari7+ */
-  filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='#0a6ada', endColorstr='#2fd22c',GradientType=1 ); /* IE6-9 fallback on horizontal gradient */
+    height: 100%;
+    margin: 0;
+    font-family: 'Montserrat', sans-serif;
+    background-repeat: no-repeat;
+    background-attachment: fixed;
+    /* Permalink - use to edit and share this gradient: http://colorzilla.com/gradient-editor/#0a6ada+0,2fd22c+100 */
+    background: #0a6ada; /* Old browsers */
+    background: -moz-linear-gradient(45deg, #0a6ada 0%, #2fd22c 100%); /* FF3.6-15 */
+    background: -webkit-linear-gradient(45deg, #0a6ada 0%,#2fd22c 100%); /* Chrome10-25,Safari5.1-6 */
+    background: linear-gradient(45deg, #0a6ada 0%,#2fd22c 100%); /* W3C, IE10+, FF16+, Chrome26+, Opera12+, Safari7+ */
+    filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='#0a6ada', endColorstr='#2fd22c',GradientType=1 ); /* IE6-9 fallback on horizontal gradient */
 }
 
 @media only screen
@@ -88,8 +34,8 @@ and (min-device-width : 768px)
 and (max-device-width : 1024px)
 and (orientation : landscape)
 {
-  body {
-    grid-template-columns: 1fr 3fr;
-  }
+    body {
+        grid-template-columns: 1fr 3fr;
+    }
 }
 </style>
