6dc0be82024ea832b4ce08d8fc1c0142deec10ca
Christian Stewart <heeler_cs@hotmail.com>
2020-01-14 21:49:28 +0000

    Moved some methods and html to components.

--- src/components/pages/PageAdmin/index.vue
+++ src/components/pages/PageAdmin/index.vue
@@ -14,51 +14,16 @@ import AdminUsers from './AdminUsers'
 export default {
     name: 'PageAdmin',
     components: { AdminNavbar, AdminUsers },
-    data: function () {
-        return {
-            users: [],
-            adminVerified: false
-        }
-    },
-    methods: {
-        getAllUsers() {
-            axios({
-                method: 'get',
-                url: '/api/users/adminGetAllUsers',
-                headers: { authorization: localStorage.getItem('jwt') }
-            })
-            .then((response) => {
-                const compare = ((a, b) => {
-                    const userA = a.email.toLowerCase()
-                    const userB = b.email.toLowerCase()
-
-                    var comparison = 0
-                    if (userA > userB) {
-                        comparison = 1
-                    } else if (userA < userB) {
-                        comparison = -1
-                    }
-
-                    return comparison
-                })
-                this.users = response.data.users.sort(compare)
-            })
-            .catch((error) => {
-                console.log(error.msg)
-            })
-        },
-    },
-    mounted() {
-        this.getAllUsers()
-    }
 }
 </script>
 
 <style>
+html, body {
+    background-color: white !important;
+}
+
 .fullscreenContainer {
     min-height: 100%;
-    display: -ms-grid;
-    display: grid;
     background-color: white;
 }
 </style>
