84b618f6828313cc9633b9c0d405133e22abfd8f
Christian Stewart <heeler_cs@hotmail.com>
2020-01-16 18:59:58 +0000

    Moved this component to a sub directory

---
+++ src/components/pages/PageAdmin/AdminUsers/index.vue
@@ -0,0 +1,178 @@
+<template>
+<div class="usersContainer">
+    <button class="addUser" @click="modalActive = true">Add User</button>
+    <div class="usersTable">
+        <div>Name</div>
+        <div>Email</div>
+        <div>Store</div>
+        <div>District</div>
+        <div>Force New Password On Next Login?</div>
+        <div>Admin?</div>
+        <div>Actions</div>
+    </div>
+    <div class="usersTable" v-for="user in users" :key="user._id">
+        <div>{{ user.firstName }} {{ user.lastName }}</div>
+        <div>{{ user.email | abbreviateEmail }}@...</div>
+        <div>{{ user.store }}</div>
+        <div>{{ user.district }}</div>
+        <div v-if="user.forceNewPasswordOnNextLogin"><i class="fas fa-check"></i></div>
+        <div v-else></div>
+        <div v-if="user.admin"><i class="fas fa-check"></i></div>
+        <div v-else></div>
+        <div class="actions">
+            <!-- <i class="far fa-edit"></i> -->
+            <i class="fas fa-trash" @click="deleteUser(user._id)"></i>
+        </div>
+    </div>
+    <modal-add-user v-if="modalActive"/>
+</div>
+</template>
+
+<script>
+import axios from 'axios'
+
+import AdminNavbar from '../AdminNavbar'
+import ModalAddUser from './ModalAddUser'
+
+export default {
+    name: 'PageAdmin',
+    components: { AdminNavbar, ModalAddUser },
+    data: function () {
+        return {
+            users: [],
+            adminVerified: false,
+            modalActive: false,
+        }
+    },
+    filters: {
+        abbreviateEmail(email) {
+            return email.split('@')[0]
+        }
+    },
+    methods: {
+        getAllUsers() {
+            axios({
+                method: 'get',
+                url: '/api/users/adminGetAllUsers',
+                headers: { authorization: 'Bearer ' + JSON.parse(localStorage.getItem('jwt')) }
+            })
+            .then(response => {
+                const compare = ((a, b) => {
+                    const userA = a.email.toLowerCase()
+                    const userB = b.email.toLowerCase()
+
+                    var comparison = 0
+                    if (userA > userB) {
+                        comparison = 1
+                    } else if (userA < userB) {
+                        comparison = -1
+                    }
+
+                    return comparison
+                })
+                this.users = response.data.users.sort(compare)
+            })
+            .catch(error => {
+                console.log(error.msg)
+            })
+        },
+        deleteUser(_id) {
+            axios({
+                method: 'delete',
+                url: '/api/users/adminDeleteUser',
+                headers: { authorization: 'Bearer ' + JSON.parse(localStorage.getItem('jwt')) },
+                data: { _id }
+            })
+            .then((response) => {
+                location.reload()
+            })
+            .catch((error) => {
+                this.createUserError = error
+            })
+        }
+    },
+    mounted() {
+        this.getAllUsers()
+    },
+}
+</script>
+
+<style lang="less">
+.usersContainer {
+    margin: 3%;
+
+    .addUser {
+        font-weight: 500;
+        font-size: 16px;
+        color: #edf0f1;
+        text-decoration: none;
+        padding: 9px 25px;
+        background-color: rgba(0, 136, 169, 1);
+        border: none;
+        border-radius: 50px;
+        cursor: pointer;
+        transition: all 0.3s ease 0s;
+
+        &:hover {
+            background-color: rgba(0, 136, 169, 0.8);
+        }
+
+        &:focus {
+            outline: none;
+        }
+    }
+
+    .usersTable {
+        display: grid;
+        grid-template-columns: repeat(auto-fill, 14%);
+        padding: 15px 10px;
+        border-bottom: 1px solid black;
+        text-align: center;
+        align-items: center;
+
+        &:first-of-type {
+            font-weight: bold;
+        }
+
+        &:nth-of-type(2n) {
+            background-color: #dedede;
+        }
+
+        &:nth-of-type(n+2) {
+            border-left: 1px solid black;
+            border-right: 1px solid black;
+        }
+
+        > .actions {
+            display: flex;
+            justify-content: center;
+        }
+
+        > .actions > i {
+            cursor: pointer;
+            margin: 0 2rem;
+        }
+
+        input {
+            appearance: none;
+            height: 1rem;
+            width: 1rem;
+            -webkit-appearance: none;
+            -moz-appearance: none;
+            -o-appearance: none;
+            appearance: none;
+            border: 1px solid black;
+            border-radius: 4px;
+            outline: none;
+            transition-duration: 0.3s;
+            background-color: none;
+            cursor: pointer;
+
+            &:checked {
+                border: 1px solid black;
+                background-color: #1F596E;
+            }
+        }
+    }
+}
+</style>
