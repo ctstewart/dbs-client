1695f232f2a926de46ddec7bb280f13818e2b693
Christian Stewart <heeler_cs@hotmail.com>
2020-01-17 21:19:30 +0000

    Added admin panel and user initials style. I added a link to the admin panel that shows up at the bottom of the sidebar if you're an admin. I also added the user's initials as a style in place of the user image.

--- src/components/layout/LayoutSidebar.vue
+++ src/components/layout/LayoutSidebar.vue
@@ -1,14 +1,22 @@
 <template>
 <div class="sidebar">
-  <div class="username"><img src="@/assets/tempUserIcon.png"><br>{{userEmail}}</div>
-  <router-link to="/" class="router-link">Benefit Sheet</router-link>
-  <router-link to="/option/optionOne" class="router-link">Option 1</router-link>
-  <router-link to="/option/optionTwo" class="router-link">Option 2</router-link>
-  <!-- <button @click="logout" type="button">Logout</button> -->
-  <div class="versionAndLogout">
-    <a class="logout" @click="logout">Logout</a>
-    <a class="versionFormatting">Version: {{webappVersion}}</a>
-  </div>
+    <!-- <div class="username"><img src="@/assets/tempUserIcon.png"><br>{{userEmail}}</div> -->
+    <div class="username">
+        <div class="userInitials">
+            <span>{{userEmail.slice(0,2).toUpperCase()}}</span>
+        </div>
+        <br>
+        {{userEmail}}
+    </div>
+    <router-link to="/" class="router-link">Benefit Sheet</router-link>
+    <router-link to="/option/optionOne" class="router-link">Option 1</router-link>
+    <router-link to="/option/optionTwo" class="router-link">Option 2</router-link>
+    <!-- <button @click="logout" type="button">Logout</button> -->
+    <div class="versionAndLogout">
+        <a class="logout" @click="logout">Logout</a>
+        <router-link v-if="admin" to="/admin">Admin Panel</router-link>
+        <a class="versionFormatting">Version: {{webappVersion}}</a>
+    </div>
 </div>
 </template>
 
@@ -16,99 +24,126 @@
 import { mapState } from 'vuex'
 
 export default {
-  name: 'Layoutsidebar',
-  computed: {
-    ...mapState([
-      'userEmail',
-      'webappVersion'
-    ])
-  },
-  methods: {
-    logout: function () {
-      sessionStorage.clear()
-      localStorage.clear()
-      this.$router.push('/login')
+    name: 'Layoutsidebar',
+    computed: {
+        ...mapState([
+            'userEmail',
+            'webappVersion',
+            'admin'
+        ])
+    },
+    methods: {
+        logout: function () {
+            sessionStorage.clear()
+            localStorage.clear()
+            this.$router.push('/login')
+        }
     }
-  }
 }
 </script>
 
-<style>
+<style lang="less">
 .sidebar {
-  background-color: rgba(0,0,0,.4);
-  height: 100%;
-  display: grid;
-  grid-template-rows: 25% 15% 15% 15% 30%;
-}
+    background-color: rgba(0,0,0,.4);
+    height: 100%;
+    display: grid;
+    grid-template-rows: 25% 15% 15% 15% 30%;
 
-.sidebar button {
-  background-color: rgba(255,255,255,.8);
-  border-radius: 10px;
-  border: none;
-  margin: 4% 5%;
-  font-size: 24px;
-  font-weight: bold;
-  cursor: pointer;
-  outline: none;
-}
+    button {
+        background-color: rgba(255,255,255,.8);
+        border-radius: 10px;
+        border: none;
+        margin: 4% 5%;
+        font-size: 24px;
+        font-weight: bold;
+        cursor: pointer;
+        outline: none;
+    }
 
-.router-link {
-  color: black;
-  display: grid;
-  align-items: center;
-  justify-items: center;
-  text-decoration: none;
-  background-color: rgba(255,255,255,.8);
-  border-radius: 10px;
-  border: none;
-  margin: 4% 5%;
-  font-size: 24px;
-  font-weight: bold;
-  cursor: pointer;
-  outline: none;
-}
+    .router-link {
+        color: black;
+        display: grid;
+        align-items: center;
+        justify-items: center;
+        text-decoration: none;
+        background-color: rgba(255,255,255,.8);
+        border-radius: 10px;
+        border: none;
+        margin: 4% 5%;
+        font-size: 24px;
+        font-weight: bold;
+        cursor: pointer;
+        outline: none;
+    }
 
-.router-link-exact-active {
-  background-color: transparent !important;
-  color: rgba(255,255,255,.8) !important;
-  border: 1px solid rgba(255,255,255,.8) !important;
-}
+    .router-link-exact-active {
+        background-color: transparent !important;
+        color: rgba(255,255,255,.8) !important;
+        border: 1px solid rgba(255,255,255,.8) !important;
+    }
 
-.username, .versionAndLogout {
-  align-self: end;
-  color: rgba(255,255,255,.8);
-  font-size: 14px;
-  /*visibility: hidden;*/
-}
+    .username, .versionAndLogout {
+        align-self: end;
+        color: rgba(255,255,255,.8);
+        font-size: 14px;
+        /*visibility: hidden;*/
+    }
 
-.username {
-  text-align: center;
-  align-self: center;
-  /*border-bottom: 1px solid rgba(255,255,255,.8);*/
-  padding-bottom: 3%;
-}
+    .username {
+        display: grid;
+        display: -ms-grid;
+        text-align: center;
+        align-self: center;
+        /*border-bottom: 1px solid rgba(255,255,255,.8);*/
+        padding-bottom: 3%;
 
-.versionAndLogout {
-  display: grid;
-  grid-template-columns: 40% 60%;
-  font-size: 1.1rem;
-  margin: 0 3% 3%;
-  cursor: pointer;
-}
+        .userInitials {
+            display: grid;
+            display: -ms-grid;
+            align-self: center;
+            justify-self: center;
+            border: 1px solid white;
+            border-radius: 5px;
+            padding: .5rem;
 
-.logout {
-  text-decoration: underline;
-}
+            span {
+                align-self: center;
+                justify-self: center;
+                font-size: 2rem;
+            }
+        }
 
-.versionFormatting {
-  text-align: right;
-}
+        img {
+            margin-bottom: 1%;
+            width: 75px;
+            height: 75px;
+            border-radius: 50%;
+            border: 1px solid rgba(255,255,255,.8);
+        }
+    }
 
-img {
-  margin-bottom: 1%;
-  width: 75px;
-  height: 75px;
-  border-radius: 50%;
-  border: 1px solid rgba(255,255,255,.8);
+    .versionAndLogout {
+        display: flex;
+        justify-content: space-between;
+        font-size: .9rem;
+        margin: 0 3% 3%;
+        cursor: pointer;
+
+        .logout {
+            text-decoration: underline;
+        }
+
+        a {
+            color: white;
+        }
+
+        .versionFormatting {
+            text-align: right;
+        }
+
+        @media (max-width: 1280px) {
+            font-size: .75rem;
+        }
+    }
 }
 </style>
