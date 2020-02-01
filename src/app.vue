805a1ffb44163654724e03ae99faaf6b7105fe43
Christian Stewart <heeler_cs@hotmail.com>
2020-02-01 23:07:12 +0000

    Moved the background color to a less stylesheet also moved it out of the body and into a container

--- src/app.vue
+++ src/app.vue
@@ -10,7 +10,9 @@ export default {
 }
 </script>
 
-<style>
+<style lang="less">
+@import './less/styles';
+
 html {
     height: 100%;
 }
@@ -19,23 +21,5 @@ body {
     height: 100%;
     margin: 0;
     font-family: 'Montserrat', sans-serif;
-    background-repeat: no-repeat;
-    background-attachment: fixed;
-    /* Permalink - use to edit and share this gradient: http://colorzilla.com/gradient-editor/#0a6ada+0,2fd22c+100 */
-    background: #0a6ada; /* Old browsers */
-    background: -moz-linear-gradient(45deg, #0a6ada 0%, #2fd22c 100%); /* FF3.6-15 */
-    background: -webkit-linear-gradient(45deg, #0a6ada 0%,#2fd22c 100%); /* Chrome10-25,Safari5.1-6 */
-    background: linear-gradient(45deg, #0a6ada 0%,#2fd22c 100%); /* W3C, IE10+, FF16+, Chrome26+, Opera12+, Safari7+ */
-    filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='#0a6ada', endColorstr='#2fd22c',GradientType=1 ); /* IE6-9 fallback on horizontal gradient */
-}
-
-@media only screen
-and (min-device-width : 768px)
-and (max-device-width : 1024px)
-and (orientation : landscape)
-{
-    body {
-        grid-template-columns: 1fr 3fr;
-    }
 }
 </style>
