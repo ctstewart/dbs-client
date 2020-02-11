5c6819a3f65d62eb24e85b01e0b40bbb696ec2a9
Christian Stewart <heeler_cs@hotmail.com>
2020-02-11 19:10:45 +0000

    Added pages for the business plans

---
+++ src/components/pages/PageBusiness/BusinessBenefitSheet/index.vue
@@ -0,0 +1,172 @@
+<template>
+<div class="BSContainer gradientBackground">
+    <layout-sidebar/>
+    <div class="mainContent" :class="[ hamburgerStyle ? 'hamburgerStyle' : 'fullStyle' ]">
+        <div class="titleBar">
+            <div class="copyButton">
+                <i class="fas fa-align-justify fa-2x" @click="hamburgerStyle = !hamburgerStyle"></i>
+            </div>
+            <p>Business</p>
+            <div class="clearButton">
+                <i class="far fa-trash-alt fa-2x" @click="resetState"></i>
+            </div>
+        </div>
+        <section-benefits/>
+        <section-old-bills/>
+        <section-bill-breakdown v-show="!hamburgerStyle"/>
+        <section-costs/>
+    </div>
+</div>
+</template>
+
+<script>
+import { createNamespacedHelpers } from 'vuex'
+const { mapState, mapGetters, mapMutations } = createNamespacedHelpers('business/benefitSheet')
+
+import jwtExpCheck from '@/mixins/jwtExpCheck'
+
+import LayoutSidebar from '@/components/layout/LayoutSidebar'
+import SectionBenefits from './SectionBenefits'
+import SectionOldBills from './SectionOldBills'
+import SectionBillBreakdown from './SectionBillBreakdown'
+import SectionCosts from './SectionCosts'
+
+export default {
+    name: 'PageBenefitSheet',
+    mixins: [jwtExpCheck],
+    components: { LayoutSidebar, SectionBenefits, SectionOldBills, SectionBillBreakdown, SectionCosts },
+    data: function () {
+        return {
+            hamburgerStyle: false,
+        }
+    },
+    methods: {
+        ...mapMutations([
+            'resetState'
+        ])
+    }
+}
+</script>
+
+<style lang="less" scoped>
+@media only screen 
+and (min-device-width : 768px) 
+and (max-device-width : 1024px) 
+and (orientation : landscape)
+{
+    .main select {
+        text-indent: 10%;
+    }
+}
+
+.displayNone {
+    display: none !important;
+}
+
+.BSContainer {
+    height: 100%;
+    display: grid;
+    grid-template-columns: 1fr 4fr;
+
+    @media print {
+        grid-template-columns: 1fr;
+    }
+
+    .fullStyle {
+        transition: all 1s;
+    }
+
+    .hamburgerStyle {
+        grid-template-rows: 10% 45% 20% 25%;
+        margin-top: 5%;
+        margin-bottom: 5%;
+        transition: all 1s;
+    }
+
+    .mainContent {
+        display: grid;
+        margin: 1%;
+        background-color: rgba(255,255,255,.9);
+        grid-template-rows: 8% 30% 10% 40% 12%;
+        border-radius: 5px;
+
+        @media print {
+            margin: 2%;
+            grid-template-rows: 35% 10% 40% 15%;
+            border: 1px solid gray;
+            border-radius: 5px;
+        }
+
+        .titleBar {
+            padding: 0 3rem;
+            margin: 0;
+            display: flex;
+            justify-content: space-between;
+            align-items: center;
+            background-color: rgba(31,89,110,.75);
+            color: #F6F6F6;
+            font-weight: bold;
+            border-bottom: 2px solid rgba(0,0,0,.5);
+            border-radius: 5px 5px 0 0;
+
+            @media print {
+                display: none !important;
+            }
+
+            p {
+                font-size: 24px;
+            }
+
+            i {
+                cursor: pointer;
+            }
+        }
+    }
+}
+
+
+@media print {
+
+  .positionRelative {
+    position: relative;
+  }
+
+  .option1 > .breakdown {
+    border-radius: 5px 0 0 5px;
+  }
+
+  .option2 > .breakdown {
+    border-left: 0;
+    border-radius: 0 5px 5px 0;
+  }
+
+  .costs {
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    margin: 0 1% 1% 1%;
+  }
+
+  .costs > div {
+    display: grid;
+    grid-template-columns: 2fr 1fr 2fr;
+    place-items: center;
+    font-weight: bold;
+  }
+
+  .costs > div > input {
+    margin: 1% 5%;
+    font-size: 16px;
+    -webkit-appearance: none;
+    border: none;
+    border: 1px solid grey;
+    background-color: white;
+    width: 96px;
+    height: 32px;
+    border-radius: 5px;
+    font-size: 16px;
+    text-align: center;
+    color: black;
+    font-weight: bold;
+  }
+}
+</style>
