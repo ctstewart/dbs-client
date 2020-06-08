e63a77d558a7eefbf1c518be5c900fc9a3d89a53
Christian Stewart <heeler_cs@hotmail.com>
2020-06-08 19:19:01 +0000

    Added dynamic PullThru and updated the component to have a for-loop

--- src/components/pages/PageOptions/PageConsumerOption/SectionPullthru.vue
+++ src/components/pages/PageOptions/PageConsumerOption/SectionPullthru.vue
@@ -1,21 +1,13 @@
 <template>
 <div class="main pullthru">
-    <div class="pullthruColumn">
-        <div v-for="(i, index) in connectedDevices.slice(0,4)" :key="i.id">
-            <p id="textAlignRight">{{i.id}}:</p>
-            <i class="fa fa-minus-circle fa-lg" aria-hidden="true" @click="decrementConnectedDevice(index)"></i>
-            <p>{{i.value}}</p>
-            <i class="fa fa-plus-circle fa-lg" aria-hidden="true" @click="incrementConnectedDevice(index)"></i>
-        </div>
-    </div>
-    <div class="pullthruColumn">
-        <div v-for="(i, index) in connectedDevices.slice(4,8)" :key="i.id">
-            <p id="textAlignRight">{{i.id}}:</p>
-            <i class="fa fa-minus-circle fa-lg" aria-hidden="true" @click="decrementConnectedDevice(index + 4)"></i>
-            <p>{{i.value}}</p>
-            <i class="fa fa-plus-circle fa-lg" aria-hidden="true" @click="incrementConnectedDevice(index + 4)"></i>
-        </div>
-    </div>
+	<div v-for="(subArray, subArrayIndex) in pullthruArraySplit" :key="subArrayIndex" class="pullthruColumn">
+		<div v-for="(i) in subArray" :key="i.id">
+			<p id="textAlignRight">{{i.id}}:</p>
+			<i class="fa fa-minus-circle fa-lg" aria-hidden="true" @click="decrementConnectedDevice(i.id)"></i>
+			<p>{{i.value}}</p>
+			<i class="fa fa-plus-circle fa-lg" aria-hidden="true" @click="incrementConnectedDevice(i.id)"></i>
+		</div>
+	</div>
 </div>
 </template>
 
@@ -28,8 +20,22 @@ export default {
 		...mapState({
 			connectedDevices (state) {
 				return state['consumer'][this.$route.params.vuexModule].connectedDevices
+			},
+			chosenPlan (state) {
+				return state['consumer'][this.$route.params.vuexModule].chosenPlan
 			}
-		})
+		}),
+		pullthruArraySplit () {
+			var localArray = this.connectedDevices
+			if (this.chosenPlan === 'Loyalty 55+') {
+				localArray = localArray.filter(i => i.id !== 'Jetpack')
+			}
+			var half = Math.ceil(localArray.length / 2)
+			var firstHalf = localArray.slice(0, half)
+			var secondHalf = localArray.slice(half)
+			localArray = [firstHalf, secondHalf]
+			return localArray
+		}
 	},
 	methods: {
 		...mapMutations({
@@ -46,44 +52,44 @@ export default {
 
 <style scoped>
 .main {
-    background-color: #387A77;
-    margin: 0 1% 1% 1%;
-    border-radius: 0 5px 5px 5px;
-    text-align: center;
-    color: white;
-    padding: 3%;
+	background-color: #387A77;
+	margin: 0 1% 1% 1%;
+	border-radius: 0 5px 5px 5px;
+	text-align: center;
+	color: white;
+	padding: 3%;
 }
 
 .pullthru {
-    display: grid;
-    grid-template-columns: 1fr 1fr;
+	display: grid;
+	grid-template-columns: 1fr 1fr;
 }
 
 .pullthruColumn {
-    display: grid;
-    border: 1px solid white;
-    margin: 0px 10px;
-    padding: 10px;
-    border-radius: 5%;
+	display: grid;
+	border: 1px solid white;
+	margin: 0px 10px;
+	padding: 10px;
+	border-radius: 5%;
 }
 
 .pullthruColumn {
-    grid-template-rows: 1fr 1fr 1fr 1fr;
-    align-items: center;
+	grid-template-rows: 1fr 1fr 1fr 1fr;
+	align-items: center;
 }
 
 .pullthruColumn div {
-    display: -ms-grid;
-    display: grid;
-    grid-template-columns: 40% 20% 20% 20%;
-    -ms-grid-row-align: center;
-    -ms-grid-column-align: center;
-    place-items: center;
-    align-items: center;
-    justify-items: center;
+	display: -ms-grid;
+	display: grid;
+	grid-template-columns: 40% 20% 20% 20%;
+	-ms-grid-row-align: center;
+	-ms-grid-column-align: center;
+	place-items: center;
+	align-items: center;
+	justify-items: center;
 }
 
 .pullthruColumn i:hover {
-    cursor: pointer;
+	cursor: pointer;
 }
 </style>
