205380103acd191850f9bb8054e30f8120c2923a
Christian Stewart <heeler_cs@hotmail.com>
2020-06-08 19:20:48 +0000

    Fixed the loyalty go plans to allow 6 lines instead of 4

--- src/store/modules/optionsModuleConsumer/getters/planTotal/loyaltyGoPlanMath.js
+++ src/store/modules/optionsModuleConsumer/getters/planTotal/loyaltyGoPlanMath.js
@@ -1,6 +1,6 @@
 const loyaltyGoPlanMath = ({ numberOfPhones, loyaltyGoPlans, autopay }) => {
 	if (numberOfPhones <= 0) { return 0 }
-	if (numberOfPhones > 4) { numberOfPhones = 4 }
+	if (numberOfPhones > 6) { numberOfPhones = 6 }
 
 	if (autopay) {
 		return loyaltyGoPlans[numberOfPhones].autopay
