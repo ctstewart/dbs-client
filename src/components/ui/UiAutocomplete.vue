b9be9a5e18919b0a0972ee6802123183ce39b795
Christian Stewart <heeler_cs@hotmail.com>
2020-06-16 17:49:17 +0000

    Added more benefits

--- src/components/ui/UiAutocomplete.vue
+++ src/components/ui/UiAutocomplete.vue
@@ -22,7 +22,7 @@ export default {
 			focus: false,
 			arrowCounter: -1,
 			items: [
-				'Protection from Damage, Loss, Theft', 'Advanced Setup', 'Better Battery', 'Better Camera', 'Bigger Screen', 'Higher Resolution', 'Wireless Charging', 'Vehicle Safety Service', 'Vehicle Diagnostics', 'Emergency Car Service', 'Fantastic Customer Service', 'Best Network Performance (RootMetrics)', 'Most Reliable Network (RootMetrics)', 'Unlimited Talk', 'Unlimited Text', 'Unlimited Data', '15GB High Speed Hotspot', '20GB High Speed Hotspot', 'Keep Your Same Phone Number', 'International Calling'
+				'Protection from Damage, Loss, Theft', 'Advanced Setup', 'Better Battery', 'Better Camera', 'Bigger Screen', 'Higher Resolution', 'Wireless Charging', 'Vehicle Safety Service', 'Vehicle Diagnostics', 'Emergency Car Service', 'Fantastic Customer Service', 'Best Network Performance (RootMetrics)', 'Most Reliable Network (RootMetrics)', 'Unlimited Talk', 'Unlimited Text', 'Unlimited Data', '15GB High Speed Hotspot', '20GB High Speed Hotspot', 'Keep Your Same Phone Number', 'International Calling', 'Bock Robocalls', 'Wifi Security', 'VPN', 'Overnight Shipping', 'Same-day Repair', '$29 Screen Repair', '$10 Screen Protector Replacements', 'Consistent Bill'
 			]
 		}
 	},
