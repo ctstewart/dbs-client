e311ae71385f3463b704538a7594a02bbaab406c
Christian Stewart <heeler_cs@hotmail.com>
2020-07-09 20:45:04 +0000

    Fixed a typo and formatted with prettier

--- src/components/ui/UiAutocomplete.vue
+++ src/components/ui/UiAutocomplete.vue
@@ -1,91 +1,139 @@
 <template>
-<div class="autocomplete" :class="{ 'positionRelative': focus === true }">
-  <input type="text" v-model="search" @input="onChange($event)" @keydown.down="onArrowDown" @keydown.up="onArrowUp" @keydown.enter="onEnter" @keydown.esc="onEsc" @focus="focus = true; onChange($event)"/>
-  <ul class="autocompleteResults" v-show="isOpen">
-    <li class="autocompleteResult" v-for="(result, i) in results" :key="i" @click="setresult($event, result)" :class="{ 'is-active': i === arrowCounter }" @mouseover="handleMouseOver">
-      {{result}}
-    </li>
-  </ul>
-</div>
+    <div class="autocomplete" :class="{ positionRelative: focus === true }">
+        <input
+            type="text"
+            v-model="search"
+            @input="onChange($event)"
+            @keydown.down="onArrowDown"
+            @keydown.up="onArrowUp"
+            @keydown.enter="onEnter"
+            @keydown.esc="onEsc"
+            @focus="
+                focus = true;
+                onChange($event);
+            "
+        />
+        <ul class="autocompleteResults" v-show="isOpen">
+            <li
+                class="autocompleteResult"
+                v-for="(result, i) in results"
+                :key="i"
+                @click="setresult($event, result)"
+                :class="{ 'is-active': i === arrowCounter }"
+                @mouseover="handleMouseOver"
+            >
+                {{ result }}
+            </li>
+        </ul>
+    </div>
 </template>
 
 <script>
 export default {
-	name: 'autocomplete',
-	props: ['benefit'],
+    name: "autocomplete",
+    props: ["benefit"],
 
-	data: function () {
-		return {
-			search: this.benefit,
-			results: [],
-			isOpen: false,
-			focus: false,
-			arrowCounter: -1,
-			items: [
-				'Protection from Damage, Loss, Theft', 'Advanced Setup', 'Better Battery', 'Better Camera', 'Bigger Screen', 'Higher Resolution', 'Wireless Charging', 'Vehicle Safety Service', 'Vehicle Diagnostics', 'Emergency Car Service', 'Fantastic Customer Service', 'Best Network Performance (RootMetrics)', 'Most Reliable Network (RootMetrics)', 'Unlimited Talk', 'Unlimited Text', 'Unlimited Data', '15GB High Speed Hotspot', '20GB High Speed Hotspot', 'Keep Your Same Phone Number', 'International Calling', 'Bock Robocalls', 'Wifi Security', 'VPN', 'Overnight Shipping', 'Same-day Repair', '$29 Screen Repair', '$10 Screen Protector Replacements', 'Consistent Bill'
-			]
-		}
-	},
+    data: function() {
+        return {
+            search: this.benefit,
+            results: [],
+            isOpen: false,
+            focus: false,
+            arrowCounter: -1,
+            items: [
+                "Protection from Damage, Loss, Theft",
+                "Advanced Setup",
+                "Better Battery",
+                "Better Camera",
+                "Bigger Screen",
+                "Higher Resolution",
+                "Wireless Charging",
+                "Vehicle Safety Service",
+                "Vehicle Diagnostics",
+                "Emergency Car Service",
+                "Fantastic Customer Service",
+                "Best Network Performance (RootMetrics)",
+                "Most Reliable Network (RootMetrics)",
+                "Unlimited Talk",
+                "Unlimited Text",
+                "Unlimited Data",
+                "15GB High Speed Hotspot",
+                "20GB High Speed Hotspot",
+                "Keep Your Same Phone Number",
+                "International Calling",
+                "Block Robocalls",
+                "Wifi Security",
+                "VPN",
+                "Overnight Shipping",
+                "Same-day Repair",
+                "$29 Screen Repair",
+                "$10 Screen Protector Replacements",
+                "Consistent Bill"
+            ]
+        };
+    },
 
-	methods: {
-		filterResults () {
-			this.results = this.items.filter(item => item.toLowerCase().indexOf(this.search.toLowerCase()) > -1)
-		},
-		onChange (event) {
-			this.filterResults()
-			if (this.results.length === 0) {
-				this.isOpen = false
-			} else if (this.results.length > 0) {
-				this.isOpen = true
-			}
-			this.$emit('changeBenefit', this.search)
-		},
-		setresult (event, result) {
-			this.isOpen = false
-			this.search = result
-			this.$emit('changeBenefit', result)
-		},
-		onArrowDown () {
-			if (this.arrowCounter < this.results.length) {
-				this.arrowCounter = this.arrowCounter + 1
-			}
-		},
-		onArrowUp () {
-			if (this.arrowCounter > -1) {
-				this.arrowCounter = this.arrowCounter - 1
-			}
-		},
-		onEnter () {
-			this.search = this.results[this.arrowCounter]
-			this.$emit('changeBenefit', this.results[this.arrowCounter])
-			this.isOpen = false
-			this.arrowCounter = -1
-		},
-		onEsc () {
-			this.isOpen = false
-			this.arrowCounter = -1
-		},
-		handleClickOutside (evt) {
-			if (!this.$el.contains(evt.target)) {
-				this.isOpen = false
-				this.arrowCounter = -1
-			}
-		},
-		handleMouseOver () {
-			if (this.arrowCounter > -1) {
-				this.arrowCounter = -1
-			}
-		}
-	},
-	computed: {
-	},
-	mounted () {
-		document.addEventListener('click', this.handleClickOutside)
-	},
-	destroyed () {
-		document.removeEventListener('click', this.handleClickOutside)
-	}
-}
+    methods: {
+        filterResults() {
+            this.results = this.items.filter(
+                item =>
+                    item.toLowerCase().indexOf(this.search.toLowerCase()) > -1
+            );
+        },
+        onChange(event) {
+            this.filterResults();
+            if (this.results.length === 0) {
+                this.isOpen = false;
+            } else if (this.results.length > 0) {
+                this.isOpen = true;
+            }
+            this.$emit("changeBenefit", this.search);
+        },
+        setresult(event, result) {
+            this.isOpen = false;
+            this.search = result;
+            this.$emit("changeBenefit", result);
+        },
+        onArrowDown() {
+            if (this.arrowCounter < this.results.length) {
+                this.arrowCounter = this.arrowCounter + 1;
+            }
+        },
+        onArrowUp() {
+            if (this.arrowCounter > -1) {
+                this.arrowCounter = this.arrowCounter - 1;
+            }
+        },
+        onEnter() {
+            this.search = this.results[this.arrowCounter];
+            this.$emit("changeBenefit", this.results[this.arrowCounter]);
+            this.isOpen = false;
+            this.arrowCounter = -1;
+        },
+        onEsc() {
+            this.isOpen = false;
+            this.arrowCounter = -1;
+        },
+        handleClickOutside(evt) {
+            if (!this.$el.contains(evt.target)) {
+                this.isOpen = false;
+                this.arrowCounter = -1;
+            }
+        },
+        handleMouseOver() {
+            if (this.arrowCounter > -1) {
+                this.arrowCounter = -1;
+            }
+        }
+    },
+    computed: {},
+    mounted() {
+        document.addEventListener("click", this.handleClickOutside);
+    },
+    destroyed() {
+        document.removeEventListener("click", this.handleClickOutside);
+    }
+};
 </script>
 
 <style lang="less" scoped>
@@ -144,7 +192,7 @@ export default {
 
             &:hover,
             &.is-active {
-                background-color: #4AAE9B;
+                background-color: #4aae9b;
                 color: white;
             }
         }
