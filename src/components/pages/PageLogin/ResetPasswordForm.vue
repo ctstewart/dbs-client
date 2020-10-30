27a97dfda3d85a8f266eeeeca2afbdfa3795e222
Christian Stewart <heeler_cs@hotmail.com>
2020-10-30 18:47:13 -0500

    Updated the password reset to use the new API

--- src/components/pages/PageLogin/ResetPasswordForm.vue
+++ src/components/pages/PageLogin/ResetPasswordForm.vue
@@ -1,12 +1,12 @@
 <template>
 <div class="passwordResetDiv">
-    <h2>Password Reset</h2>
-    <input-field v-bind="{inputStyle: {initialInputType: 'text', label:'Email', placeholder:'johndoe@gmail.com', meter:false}}" :value="email" v-on:value-changed="$emit('change-email', $event)" v-on:keyupEnter="sendEmailForPasswordReset"/>
-    <div></div>
-    <div class="buttonGroup">
-        <button @click="$emit('change-to-form', 'LoginForm')">Go Back</button>
-        <button @click="sendEmailForPasswordReset">RESET</button>
-    </div>
+	<h2>Password Reset</h2>
+	<input-field v-bind="{inputStyle: {initialInputType: 'text', label:'Email', placeholder:'johndoe@gmail.com', meter:false}}" :value="email" v-on:value-changed="$emit('change-email', $event)" v-on:keyupEnter="sendEmailForPasswordReset"/>
+	<div></div>
+	<div class="buttonGroup">
+		<button @click="$emit('change-to-form', 'LoginForm')">Go Back</button>
+		<button @click="sendEmailForPasswordReset">RESET</button>
+	</div>
 </div>
 </template>
 
@@ -20,18 +20,18 @@ export default {
 	props: ['email'],
 	components: { InputField },
 	methods: {
-		sendEmailForPasswordReset: function () {
-			axios.post('/api/users/sendPasswordResetEmail', {
-				'email': this.email
-			})
-				.then((response) => {
-					console.log(response.data.msg)
-					this.$emit('change-to-form', 'ResetPasswordConfirmation')
+		async sendEmailForPasswordReset () {
+			try {
+				await axios({
+					method: 'post',
+					url: `${process.env.VUE_APP_API_URL}/api/v1/auth/forgotpassword`,
+					data: { email: this.email }
 				})
-				.catch((error) => {
-					console.log('API Error')
-					console.log(error)
-				})
+
+				this.$emit('change-to-form', 'ResetPasswordConfirmation')
+			} catch (err) {
+				console.error(err)
+			}
 		}
 	}
 }
@@ -39,58 +39,58 @@ export default {
 
 <style lang="less">
 .passwordResetDiv {
-    display: -ms-grid;
-    display: grid;
-    grid-template-rows: 15% 30% 30% 25%;
-    border-right: 1px solid grey;
+	display: -ms-grid;
+	display: grid;
+	grid-template-rows: 15% 30% 30% 25%;
+	border-right: 1px solid grey;
 
-    > div {
-        display: grid;
-    }
+	> div {
+		display: grid;
+	}
 
-    button, span {
-        align-self: center;
-        justify-self: end;
-    }
+	button, span {
+		align-self: center;
+		justify-self: end;
+	}
 
-    button {
-        color: white;
-        background-color: #1F596E;
-        border-radius: 5px;
-        width: 7rem;
-        height: 3.5rem;
-        border: none;
-        font-weight: bold;
-        font-size: 20px;
-        outline: none;
-        cursor: pointer;
-    }
+	button {
+		color: white;
+		background-color: #1F596E;
+		border-radius: 5px;
+		width: 7rem;
+		height: 3.5rem;
+		border: none;
+		font-weight: bold;
+		font-size: 20px;
+		outline: none;
+		cursor: pointer;
+	}
 
-    span {
-        color: red;
-    }
+	span {
+		color: red;
+	}
 
-    .forgotPassword {
-        padding-top: 1%;
-        font-size: 14px;
-        display: grid;
-        align-items: center;
-        color: #1F596E;
-        text-decoration: underline;
-        cursor: pointer;
-    }
+	.forgotPassword {
+		padding-top: 1%;
+		font-size: 14px;
+		display: grid;
+		align-items: center;
+		color: #1F596E;
+		text-decoration: underline;
+		cursor: pointer;
+	}
 }
 
 .buttonGroup {
-    justify-self: end;
-    display: grid;
-    grid-template-columns: 1fr 1fr;
+	justify-self: end;
+	display: grid;
+	grid-template-columns: 1fr 1fr;
 
-    > button:first-child {
-        margin-right: .25rem;
-        background-color: white;
-        color: #1F596E;
-        border: 1px solid #1F596E;
-    }
+	> button:first-child {
+		margin-right: .25rem;
+		background-color: white;
+		color: #1F596E;
+		border: 1px solid #1F596E;
+	}
 }
 </style>
--- src/components/pages/PageResetPassword.vue
+++ src/components/pages/PageResetPassword.vue
@@ -1,15 +1,15 @@
 <template>
 <div class="newPasswordContainer gradientBackground">
-    <div class="newPasswordDiv">
-        <div class="newPasswordFormDiv">
-            <h2>New Password</h2>
-            <input-field v-bind="{inputStyle: {initialInputType: 'password', label:'Password', placeholder:'password', meter:true}}" :value="password" v-on:value-changed="password = $event"/>
-            <input-field v-bind="{inputStyle: {initialInputType: 'password', label:'Confirm Password', placeholder:'password', meter:false}}" :value="confirmPassword" v-on:value-changed="confirmPassword = $event"/>
-            <span v-if="errorText" class="errorText">{{ errorText }}</span>
-            <span v-else-if="successText" class="successText">{{ successText }}</span>
-            <button v-else @click="resetPassword">SUBMIT</button>
-        </div>
-    </div>
+	<div class="newPasswordDiv">
+		<div class="newPasswordFormDiv">
+			<h2>New Password</h2>
+			<input-field v-bind="{inputStyle: {initialInputType: 'password', label:'Password', placeholder:'password', meter:true}}" :value="password" v-on:value-changed="password = $event"/>
+			<input-field v-bind="{inputStyle: {initialInputType: 'password', label:'Confirm Password', placeholder:'password', meter:false}}" :value="confirmPassword" v-on:value-changed="confirmPassword = $event"/>
+			<span v-if="errorText" class="errorText">{{ errorText }}</span>
+			<span v-else-if="successText" class="successText">{{ successText }}</span>
+			<button v-else @click="resetPassword">SUBMIT</button>
+		</div>
+	</div>
 </div>
 </template>
 
@@ -28,14 +28,15 @@ export default {
 			password: '',
 			confirmPassword: '',
 			errorText: '',
-			successText: ''
+			successText: '',
+			resetToken: this.$route.params.resetToken
 		}
 	},
 	methods: {
 		...mapMutations([
 			'mutate'
 		]),
-		resetPassword: function () {
+		async resetPassword () {
 			if (this.password !== this.confirmPassword) {
 				this.errorText = 'Passwords do not match'
 				setTimeout(() => {
@@ -45,98 +46,93 @@ export default {
 				this.errorText = 'Please create a stronger password'
 				setTimeout(() => {
 					this.errorText = ''
-					this.$router.push('/login')
 				}, 2000)
 			} else {
-				axios({
-					method: 'post',
-					url: '/api/users/resetPassword',
-					data: { password: this.password },
-					headers: { authorization: `Bearer: ${this.$route.params.resetToken}` }
-				})
-					.then((response) => {
-						this.successText = response.data.msg
-						setTimeout(() => {
-							this.$router.push('/login')
-						}, 4000)
-					})
-					.catch((error) => {
-						if (error.msg) {
-							this.errorText = error.msg
-						} else {
-							this.errorText = 'Unknown Error. Please try again.'
-						}
-						setTimeout(() => {
-							this.errorText = ''
-						}, 4000)
-					})
+				try {
+					await axios({
+						method: 'put',
+						url: `${process.env.VUE_APP_API_URL}/api/v1/auth/resetpassword/${this.resetToken}`,
+						data: { password: this.password },
+					})                   
+
+					this.successText = 'Password Reset!'
+					setTimeout(() => {
+						this.$router.push('/login')
+					}, 4000)
+				} catch (err) {
+					console.error(err)
+					if (err.msg) {
+						this.errorText = err.msg
+					} else {
+						this.errorText = 'Unknown Error. Please try again.'
+					}
+					setTimeout(() => {
+						this.errorText = ''
+					}, 4000)
+				}
 			}
 		},
-		saveTokenToLocalStorage: function (value) {
-			const parsed = JSON.stringify(value)
-			localStorage.setItem('jwt', parsed)
-		}
 	}
 }
 </script>
 
 <style lang="less">
 .newPasswordContainer {
-    height: 100%;
-    display: -ms-grid;
-    display: grid;
+	height: 100%;
+	display: -ms-grid;
+	display: grid;
 
-    .newPasswordDiv {
-        display: -ms-grid;
-        display: grid;
-        width: 50%;
-        height: 50%;
-        margin: 0 auto;
-        -ms-grid-row-align: center;
-        -ms-grid-column-align: center;
-        place-self: center;
-        align-self: center;
-        justify-self: center;
-        border-radius: 5px;
-        background-color: white;
+	.newPasswordDiv {
+		display: -ms-grid;
+		display: grid;
+		width: 50%;
+		height: 50%;
+		margin: 0 auto;
+		-ms-grid-row-align: center;
+		-ms-grid-column-align: center;
+		place-self: center;
+		align-self: center;
+		justify-self: center;
+		border-radius: 5px;
+		background-color: white;
 
-        > div {
-            padding: 1rem;
-        }
+		> div {
+			padding: 1rem;
+		}
 
-        .newPasswordFormDiv {
-            display: -ms-grid;
-            display: grid;
-            grid-template-rows: 15% 30% 30% 25%;
+		.newPasswordFormDiv {
+			display: -ms-grid;
+			display: grid;
+			grid-template-rows: 15% 30% 30% 25%;
 
-            > div {
-                display: -ms-grid;
-                display: grid;
-            }
+			> div {
+				display: -ms-grid;
+				display: grid;
+			}
 
-            button, span {
-                align-self: center;
-                justify-self: end;
-            }
+			button, span {
+				align-self: center;
+				justify-self: end;
+			}
 
-            button {
-                color: white;
-                background-color: #1F596E;
-                border-radius: 5px;
-                width: 7rem;
-                height: 3.5rem;
-                border: none;
-                font-weight: bold;
-                font-size: 20px;
-                outline: none;
-                cursor: pointer;
-            }
+			button {
+				color: white;
+				background-color: #1F596E;
+				border-radius: 5px;
+				width: 7rem;
+				height: 3.5rem;
+				border: none;
+				font-weight: bold;
+				font-size: 20px;
+				outline: none;
+				cursor: pointer;
+			}
 
-            span {
-                color: red;
-            }
+			span {
+				color: red;
+			}
 
-        }
-    }
+		}
+	}
 }
 </style>
