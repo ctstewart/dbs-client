a50ff4a426e77494c56afa793c7c614257269bc2
Christian Stewart <heeler_cs@hotmail.com>
2020-11-03 16:06:35 -0600

    Moved styles to the parent component; lint

--- src/components/ui/UiAutocomplete.vue
+++ src/components/ui/UiAutocomplete.vue
@@ -3,6 +3,7 @@
 		<input
 			type="text"
 			v-model="search"
+			:style="autocompleteInputStyleObject"
 			@input="onChange($event)"
 			@keydown.down="onArrowDown"
 			@keydown.up="onArrowUp"
@@ -13,7 +14,7 @@
 				onChange($event);
 			"
 		/>
-		<ul class="autocompleteResults" v-show="isOpen">
+		<ul class="autocompleteResults" :style="autocompleteResultsStyleObject" v-show="isOpen">
 			<li
 				class="autocompleteResult"
 				v-for="(result, i) in results"
@@ -31,7 +32,13 @@
 <script>
 export default {
 	name: "autocomplete",
-	props: ["benefit", "items"],
+	props: ["benefit", "items", "autocompleteInputStyleObject", "autocompleteResultsStyleObject"],
+
+	watch: {
+		benefit (newValue) {
+			this.search = newValue
+		}
+	},
 
 	data: function() {
 		return {
@@ -48,61 +55,61 @@ export default {
 			this.results = this.items.filter(
 				item =>
 					item.toLowerCase().indexOf(this.search.toLowerCase()) > -1
-			);
+			)
 		},
 		onChange(event) {
 			this.filterResults();
 			if (this.results.length === 0) {
-				this.isOpen = false;
+				this.isOpen = false
 			} else if (this.results.length > 0) {
-				this.isOpen = true;
+				this.isOpen = true
 			}
-			this.$emit("changeBenefit", this.search);
+			this.$emit("changeBenefit", this.search)
 		},
 		setresult(event, result) {
-			this.isOpen = false;
-			this.search = result;
-			this.$emit("changeBenefit", result);
+			this.isOpen = false
+			this.search = result
+			this.$emit("changeBenefit", result)
 		},
 		onArrowDown() {
 			if (this.arrowCounter < this.results.length) {
-				this.arrowCounter = this.arrowCounter + 1;
+				this.arrowCounter = this.arrowCounter + 1
 			}
 		},
 		onArrowUp() {
 			if (this.arrowCounter > -1) {
-				this.arrowCounter = this.arrowCounter - 1;
+				this.arrowCounter = this.arrowCounter - 1
 			}
 		},
 		onEnter() {
-			this.search = this.results[this.arrowCounter];
-			this.$emit("changeBenefit", this.results[this.arrowCounter]);
-			this.isOpen = false;
-			this.arrowCounter = -1;
+			this.search = this.results[this.arrowCounter]
+			this.$emit("changeBenefit", this.results[this.arrowCounter])
+			this.isOpen = false
+			this.arrowCounter = -1
 		},
 		onEsc() {
-			this.isOpen = false;
-			this.arrowCounter = -1;
+			this.isOpen = false
+			this.arrowCounter = -1
 		},
 		handleClickOutside(evt) {
 			if (!this.$el.contains(evt.target)) {
-				this.isOpen = false;
-				this.arrowCounter = -1;
+				this.isOpen = false
+				this.arrowCounter = -1
 			}
 		},
 		handleMouseOver() {
 			if (this.arrowCounter > -1) {
-				this.arrowCounter = -1;
+				this.arrowCounter = -1
 			}
 		}
 	},
 	mounted() {
-		document.addEventListener("click", this.handleClickOutside);
+		document.addEventListener("click", this.handleClickOutside)
 	},
 	destroyed() {
-		document.removeEventListener("click", this.handleClickOutside);
+		document.removeEventListener("click", this.handleClickOutside)
 	}
-};
+}
 </script>
 
 <style lang="less" scoped>
@@ -116,16 +123,12 @@ export default {
 	align-items: center;
 
 	input {
-		width: 80%;
 		-webkit-appearance: none;
 		border-radius: 0;
 		border: none;
-		border-bottom: 1px solid black;
-		background-color: silver;
-		font-size: 16px;
+		font-size: 12px;
 		text-align: center;
 		font-weight: bold;
-		color: black;
 
 		@media print {
 			font-size: 18px;
@@ -134,16 +137,13 @@ export default {
 
 		&:focus {
 			outline: none;
-			border: 1px solid blue;
 		}
 	}
 
 	.autocompleteResults {
 		position: absolute;
 		top: 100%;
-		left: 10%;
 		background-color: white;
-		width: 80%;
 		align-self: start;
 		padding: 0;
 		margin: 0;
