1a288bffc1c6db8f5b0d063133113e4a432132f0
Christian Stewart <heeler_cs@hotmail.com>
2021-03-12 18:15:07 -0600

    Changed the order of mix and match plans

--- src/store/modules/optionsModuleConsumer/defaultState.js
+++ src/store/modules/optionsModuleConsumer/defaultState.js
@@ -4,22 +4,13 @@ const defaultState = () => {
 		mixAndMatchPlans: {
 			mixAndMatch3: [
 				{
-					id: "Start 3.0",
+					id: "Get More 3.0",
 					numberOfPhones: 0,
-					1: { lines: 1, autopay: 70, noAutopay: 80 },
-					2: { lines: 2, autopay: 60, noAutopay: 70 },
-					3: { lines: 3, autopay: 45, noAutopay: 55 },
-					4: { lines: 4, autopay: 35, noAutopay: 45 },
-					5: { lines: 5, autopay: 30, noAutopay: 40 }
-				},
-				{
-					id: "Play More 3.0",
-					numberOfPhones: 0,
-					1: { lines: 1, autopay: 80, noAutopay: 90 },
-					2: { lines: 2, autopay: 70, noAutopay: 80 },
-					3: { lines: 3, autopay: 55, noAutopay: 65 },
-					4: { lines: 4, autopay: 45, noAutopay: 55 },
-					5: { lines: 5, autopay: 40, noAutopay: 50 }
+					1: { lines: 1, autopay: 90, noAutopay: 100 },
+					2: { lines: 2, autopay: 80, noAutopay: 90 },
+					3: { lines: 3, autopay: 65, noAutopay: 75 },
+					4: { lines: 4, autopay: 55, noAutopay: 65 },
+					5: { lines: 5, autopay: 50, noAutopay: 60 }
 				},
 				{
 					id: "Do More 3.0",
@@ -31,13 +22,22 @@ const defaultState = () => {
 					5: { lines: 5, autopay: 40, noAutopay: 50 }
 				},
 				{
-					id: "Get More 3.0",
+					id: "Play More 3.0",
 					numberOfPhones: 0,
-					1: { lines: 1, autopay: 90, noAutopay: 100 },
-					2: { lines: 2, autopay: 80, noAutopay: 90 },
-					3: { lines: 3, autopay: 65, noAutopay: 75 },
-					4: { lines: 4, autopay: 55, noAutopay: 65 },
-					5: { lines: 5, autopay: 50, noAutopay: 60 }
+					1: { lines: 1, autopay: 80, noAutopay: 90 },
+					2: { lines: 2, autopay: 70, noAutopay: 80 },
+					3: { lines: 3, autopay: 55, noAutopay: 65 },
+					4: { lines: 4, autopay: 45, noAutopay: 55 },
+					5: { lines: 5, autopay: 40, noAutopay: 50 }
+				},
+				{
+					id: "Start 3.0",
+					numberOfPhones: 0,
+					1: { lines: 1, autopay: 70, noAutopay: 80 },
+					2: { lines: 2, autopay: 60, noAutopay: 70 },
+					3: { lines: 3, autopay: 45, noAutopay: 55 },
+					4: { lines: 4, autopay: 35, noAutopay: 45 },
+					5: { lines: 5, autopay: 30, noAutopay: 40 }
 				},
 				{
 					id: "Just Kids (New)",
@@ -51,22 +51,13 @@ const defaultState = () => {
 			],
 			mixAndMatch2: [
 				{
-					id: "Start",
+					id: "Get More",
 					numberOfPhones: 0,
-					1: { lines: 1, autopay: 70, noAutopay: 80 },
-					2: { lines: 2, autopay: 60, noAutopay: 70 },
-					3: { lines: 3, autopay: 45, noAutopay: 55 },
-					4: { lines: 4, autopay: 35, noAutopay: 45 },
-					5: { lines: 5, autopay: 30, noAutopay: 40 }
-				},
-				{
-					id: "Play More",
-					numberOfPhones: 0,
-					1: { lines: 1, autopay: 80, noAutopay: 90 },
-					2: { lines: 2, autopay: 70, noAutopay: 80 },
-					3: { lines: 3, autopay: 55, noAutopay: 65 },
-					4: { lines: 4, autopay: 45, noAutopay: 55 },
-					5: { lines: 5, autopay: 40, noAutopay: 50 }
+					1: { lines: 1, autopay: 90, noAutopay: 100 },
+					2: { lines: 2, autopay: 80, noAutopay: 90 },
+					3: { lines: 3, autopay: 65, noAutopay: 75 },
+					4: { lines: 4, autopay: 55, noAutopay: 65 },
+					5: { lines: 5, autopay: 50, noAutopay: 60 }
 				},
 				{
 					id: "Do More",
@@ -78,24 +69,33 @@ const defaultState = () => {
 					5: { lines: 5, autopay: 40, noAutopay: 50 }
 				},
 				{
-					id: "Get More",
+					id: "Play More",
 					numberOfPhones: 0,
-					1: { lines: 1, autopay: 90, noAutopay: 100 },
-					2: { lines: 2, autopay: 80, noAutopay: 90 },
-					3: { lines: 3, autopay: 65, noAutopay: 75 },
-					4: { lines: 4, autopay: 55, noAutopay: 65 },
-					5: { lines: 5, autopay: 50, noAutopay: 60 }
+					1: { lines: 1, autopay: 80, noAutopay: 90 },
+					2: { lines: 2, autopay: 70, noAutopay: 80 },
+					3: { lines: 3, autopay: 55, noAutopay: 65 },
+					4: { lines: 4, autopay: 45, noAutopay: 55 },
+					5: { lines: 5, autopay: 40, noAutopay: 50 }
+				},
+				{
+					id: "Start",
+					numberOfPhones: 0,
+					1: { lines: 1, autopay: 70, noAutopay: 80 },
+					2: { lines: 2, autopay: 60, noAutopay: 70 },
+					3: { lines: 3, autopay: 45, noAutopay: 55 },
+					4: { lines: 4, autopay: 35, noAutopay: 45 },
+					5: { lines: 5, autopay: 30, noAutopay: 40 }
 				},
 			],
 			mixAndMatch1: [
 				{
-					id: "Go",
+					id: "Above",
 					numberOfPhones: 0,
-					1: { lines: 1, autopay: 75, noAutopay: 80 },
-					2: { lines: 2, autopay: 65, noAutopay: 70 },
-					3: { lines: 3, autopay: 50, noAutopay: 55 },
-					4: { lines: 4, autopay: 40, noAutopay: 45 },
-					5: { lines: 5, autopay: 40, noAutopay: 45 }
+					1: { lines: 1, autopay: 95, noAutopay: 100 },
+					2: { lines: 2, autopay: 90, noAutopay: 95 },
+					3: { lines: 3, autopay: 70, noAutopay: 75 },
+					4: { lines: 4, autopay: 60, noAutopay: 65 },
+					5: { lines: 5, autopay: 60, noAutopay: 65 }
 				},
 				{
 					id: "Beyond",
@@ -107,13 +107,13 @@ const defaultState = () => {
 					5: { lines: 5, autopay: 50, noAutopay: 55 }
 				},
 				{
-					id: "Above",
+					id: "Go",
 					numberOfPhones: 0,
-					1: { lines: 1, autopay: 95, noAutopay: 100 },
-					2: { lines: 2, autopay: 90, noAutopay: 95 },
-					3: { lines: 3, autopay: 70, noAutopay: 75 },
-					4: { lines: 4, autopay: 60, noAutopay: 65 },
-					5: { lines: 5, autopay: 60, noAutopay: 65 }
+					1: { lines: 1, autopay: 75, noAutopay: 80 },
+					2: { lines: 2, autopay: 65, noAutopay: 70 },
+					3: { lines: 3, autopay: 50, noAutopay: 55 },
+					4: { lines: 4, autopay: 40, noAutopay: 45 },
+					5: { lines: 5, autopay: 40, noAutopay: 45 }
 				},
 				{
 					id: "Just Kids (Old)",
