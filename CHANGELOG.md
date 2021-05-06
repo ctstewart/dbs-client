ab1db1c9f8ccf2a9c0fbe2d668b8a22a251234b1
Christian Stewart <heeler_cs@hotmail.com>
2021-05-06 14:13:01 -0500

    client 4.1.0

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -4,15 +4,12 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
-## [4.1.0]
-### Added
-- 30 month DPP option
-
+## [4.1.0] - 2021-05-06
 ### Changed
 - $40 activation is now $35 activation
 
 ### Fixed
-- Fixed the military discount on the $110 Unlimited plan
+- Fixed the discounts on the 5GB, 10GB and $110 Unlimited plans
 
 ## [4.0.0] - 2021-03-30
 ### Added
--- src/components/pages/PageBenefitSheet/SectionBenefits.vue
+++ src/components/pages/PageBenefitSheet/SectionBenefits.vue
@@ -1,102 +1,108 @@
 <template>
-<div class="benefits">
-    <p class="benefitsTitle">New Plan Benefits</p>
-    <ui-autocomplete v-for="(i, index) in benefits" :key="index" :benefit="i" :items="items" v-on:changeBenefit="mutateBenefits({index, value: $event})" :autocompleteInputStyleObject="autocompleteInputStyleObject" :autocompleteResultsStyleObject="autocompleteResultsStyleObject"/>
-</div>
+	<div class="benefits">
+		<p class="benefitsTitle">New Plan Benefits</p>
+		<ui-autocomplete
+			v-for="(i, index) in benefits"
+			:key="index"
+			:benefit="i"
+			:items="items"
+			v-on:changeBenefit="mutateBenefits({ index, value: $event })"
+			:autocompleteInputStyleObject="autocompleteInputStyleObject"
+			:autocompleteResultsStyleObject="autocompleteResultsStyleObject"
+		/>
+	</div>
 </template>
 
 <script>
-import { createNamespacedHelpers } from 'vuex'
+import { createNamespacedHelpers } from "vuex";
 
-import UiAutocomplete from '@/components/ui/UiAutocomplete'
-const { mapState, mapMutations } = createNamespacedHelpers('benefitSheet')
+import UiAutocomplete from "@/components/ui/UiAutocomplete";
+const { mapState, mapMutations } = createNamespacedHelpers("benefitSheet");
 
 export default {
-	name: 'SectionBenefits',
-    components: { UiAutocomplete },
-    data() {
-        return {
+	name: "SectionBenefits",
+	components: { UiAutocomplete },
+	data() {
+		return {
 			items: [
-                'Protection from Damage, Loss, Theft',
-                'Advanced Setup',
-                'Better Battery',
-                'Better Camera',
-                'Bigger Screen',
-                'Higher Resolution',
-                'Wireless Charging',
-                'Vehicle Safety Service',
-                'Vehicle Diagnostics',
-                'Emergency Car Service',
-                'Fantastic Customer Service',
-                'Best Network Performance (RootMetrics)',
-                'Most Reliable Network (RootMetrics)',
-                'Unlimited Talk',
-                'Unlimited Text',
-                'Unlimited Data',
-                '15GB High Speed Hotspot',
-                '20GB High Speed Hotspot',
-                'Keep Your Same Phone Number',
-                'International Calling',
-                'Disney+, Hulu, ESPN+',
-                "Block Robocalls",
-                "Wifi Security",
-                "VPN",
-                "Overnight Shipping",
-                "Same-day Repair",
-                "$29 Screen Repair",
-                "$10 Screen Protector Replacements",
-                "Consistent Bill"
-            ],
-            autocompleteInputStyleObject: {
-                'width': '80%',
-                'border-bottom': '1px solid black',
-                'background-color': 'silver',
-                'font-size': '16px',
-                'color': 'black'
-            },
-            autocompleteResultsStyleObject: {
-                'left': '10%',
-                'width': '80%'
-            }
-        }
-    },
+				"Protection from Damage, Loss, Theft",
+				"Advanced Setup",
+				"Better Battery",
+				"Better Camera",
+				"Bigger Screen",
+				"Higher Resolution",
+				"Wireless Charging",
+				"Vehicle Safety Service",
+				"Vehicle Diagnostics",
+				"Emergency Car Service",
+				"Fantastic Customer Service",
+				"Best Network Performance (RootMetrics)",
+				"Most Reliable Network (RootMetrics)",
+				"Unlimited Talk",
+				"Unlimited Text",
+				"Unlimited Data",
+				"15GB High Speed Hotspot",
+				"20GB High Speed Hotspot",
+				"Keep Your Same Phone Number",
+				"International Calling",
+				"Disney+, Hulu, ESPN+",
+				"Block Robocalls",
+				"Wifi Security",
+				"VPN",
+				"Overnight Shipping",
+				"Same-day Repair",
+				"$29 Screen Repair",
+				"$10 Screen Protector Replacements",
+				"Consistent Bill",
+				"Call Filter App",
+				"Digital Secure App"
+			],
+			autocompleteInputStyleObject: {
+				width: "80%",
+				"border-bottom": "1px solid black",
+				"background-color": "silver",
+				"font-size": "16px",
+				color: "black"
+			},
+			autocompleteResultsStyleObject: {
+				left: "10%",
+				width: "80%"
+			}
+		};
+	},
 	computed: {
-		...mapState([
-			'benefits'
-		])
+		...mapState(["benefits"])
 	},
 	methods: {
-		...mapMutations([
-			'mutateBenefits'
-		])
+		...mapMutations(["mutateBenefits"])
 	}
-}
+};
 </script>
 
 <style lang="less" scoped>
 .benefits {
-    margin: 1%;
-    border-radius: 5px;
-    padding: 1%;
-    background-color: silver;
-    display: grid;
-    grid-template-columns: 1fr 1fr;
-    grid-template-rows: 1fr 2fr 2fr 2fr;
+	margin: 1%;
+	border-radius: 5px;
+	padding: 1%;
+	background-color: silver;
+	display: grid;
+	grid-template-columns: 1fr 1fr;
+	grid-template-rows: 1fr 2fr 2fr 2fr;
 
-    @media print {
-        background-color: lightgray;
-    }
+	@media print {
+		background-color: lightgray;
+	}
 
-    .benefitsTitle {
-        margin: 0;
-        font-size: 16px;
-        grid-column: 1 / span 2;
-        align-self: start;
-        text-align: center;
+	.benefitsTitle {
+		margin: 0;
+		font-size: 16px;
+		grid-column: 1 / span 2;
+		align-self: start;
+		text-align: center;
 
-        @media print {
-            color: red;
-        }
-    }
+		@media print {
+			color: red;
+		}
+	}
 }
 </style>
